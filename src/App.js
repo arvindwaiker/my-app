cc7dbb7e274499a75a0f6550ae11c05dab7e31f8
arvindwaiker <arvindwaiker@gmail.com>
2020-03-07 17:19:20 +0530

    Adding Clock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome.js';
+import Clock from './Clock.js'
 
 function App() {
   let user = {
@@ -11,21 +12,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Welcome name={user.firstname}/>
+        <Clock />
         <p>
           Built by {user.firstname + ' ' + user.lastname}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
